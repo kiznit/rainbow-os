a53c8c4027db2a2a33291c4d45f1fbb479723048
Thierry Tremblay <kiznit@users.noreply.github.com>
2017-02-04 14:17:49 -0800

    Loading of initrd in EFI bootloader.

--- boot/efi/efi.cpp
+++ boot/efi/efi.cpp
@@ -27,11 +27,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Uefi.h>
+#include <Guid/FileInfo.h>
+#include <Protocol/LoadedImage.h>
+#include <Protocol/SimpleFileSystem.h>
 
 #include "boot.hpp"
 
+static EFI_GUID g_efiFileInfoGuid = EFI_FILE_INFO_ID;
+static EFI_GUID g_efiLoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+static EFI_GUID g_efiSimpleFileSystemProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
 
 static EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* g_console;
+static EFI_HANDLE                       g_efiImage;
+static EFI_SYSTEM_TABLE*                g_efiSystemTable;
+static EFI_BOOT_SERVICES*               g_efiBootServices;
+static EFI_RUNTIME_SERVICES*            g_efiRuntimeServices;
 
 
 
@@ -117,10 +127,71 @@ static void InitConsole(EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* console)
 
 
 
+static EFI_STATUS LoadInitrd(EFI_FILE_PROTOCOL* fileSystemRoot, const wchar_t* path)
+{
+    EFI_FILE_PROTOCOL* file = NULL;
+    EFI_FILE_INFO* info = NULL;
+    void* initrd = NULL;
+    UINTN size;
+    EFI_STATUS status;
+
+    // Open the initrd file
+    status = fileSystemRoot->Open(fileSystemRoot, &file, (CHAR16*)path, EFI_FILE_MODE_READ, 0);
+    if (EFI_ERROR(status))
+        goto error;
+
+    // Retrieve the initrd's size
+    size = 0;
+    status = file->GetInfo(file, &g_efiFileInfoGuid, &size, NULL);
+    if (EFI_ERROR(status) && status != EFI_BUFFER_TOO_SMALL)
+        goto error;
+
+    status = g_efiBootServices->AllocatePool(EfiLoaderData, size, (void**)&info);
+    if (EFI_ERROR(status))
+        goto error;
+
+    status = file->GetInfo(file, &g_efiFileInfoGuid, &size, info);
+    if (EFI_ERROR(status))
+        goto error;
+
+    // Allocate memory to hold the initrd
+    status = g_efiBootServices->AllocatePool(EfiLoaderData, info->FileSize, &initrd);
+    if (EFI_ERROR(status))
+        goto error;
+
+    // Read the initrd into memory
+    size = info->FileSize;
+    status = file->Read(file, &size, initrd);
+    if (EFI_ERROR(status) || size != info->FileSize)
+        goto error;
+
+    printf("initrd address: %p\n", initrd);
+    printf("initrd size   : %p\n", (void*)size);
+
+    goto exit;
+
+error:
+    if (initrd) g_efiBootServices->FreePool(initrd);
+
+exit:
+    if (info) g_efiBootServices->FreePool(info);
+    if (file) file->Close(file);
+
+    return status;
+}
+
+
+
 extern "C" EFI_STATUS EFIAPI efi_main(EFI_HANDLE hImage, EFI_SYSTEM_TABLE* systemTable)
 {
     if (!hImage || !systemTable)
         return EFI_INVALID_PARAMETER;
+
+    // Keep these around, they are useful
+    g_efiImage = hImage;
+    g_efiSystemTable = systemTable;
+    g_efiBootServices = systemTable->BootServices;
+    g_efiRuntimeServices = systemTable->RuntimeServices;
 
     // Welcome message
     EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL* console = systemTable->ConOut;
@@ -142,11 +213,36 @@ extern "C" EFI_STATUS EFIAPI efi_main(EFI_HANDLE hImage, EFI_SYSTEM_TABLE* syste
     }
 
 
+    EFI_LOADED_IMAGE_PROTOCOL* image = NULL;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs = NULL;
+    EFI_FILE_PROTOCOL* fileSystemRoot = NULL;
+    EFI_STATUS status;
+
+    status = g_efiBootServices->HandleProtocol(g_efiImage, &g_efiLoadedImageProtocolGuid, (void**)&image);
+    if (EFI_ERROR(status) || !image)
+        goto error;
+
+    status = g_efiBootServices->HandleProtocol(image->DeviceHandle, &g_efiSimpleFileSystemProtocolGuid, (void**)&fs);
+    if (EFI_ERROR(status) || !fs)
+        goto error;
+
+    status = fs->OpenVolume(fs, &fileSystemRoot);
+    if (EFI_ERROR(status))
+        goto error;
+
+    status = LoadInitrd(fileSystemRoot, L"\\EFI\\rainbow\\initrd.img");
+    if (EFI_ERROR(status))
+        goto error;
+
+error:
+//exit:
+    if (fileSystemRoot) fileSystemRoot->Close(fileSystemRoot);
+
     for (;;);
 
     // printf("\nPress any key to exit");
     // getchar();
     // printf("\nExiting...");
 
-    return EFI_SUCCESS;
+    return status;
 }
