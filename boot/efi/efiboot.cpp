64c885a2865fa5dad56b83ef3ef052a4d0ef41d1
Thierry Tremblay <kiznit@users.noreply.github.com>
2018-12-05 22:57:55 -0800

    Move display related functions to efidisplay.cpp

--- boot/efi/efiboot.cpp
+++ boot/efi/efiboot.cpp
@@ -27,32 +27,23 @@
 #include "boot.hpp"
 #include <uefi.h>
 #include <Guid/FileInfo.h>
-#include <Protocol/EdidActive.h>
-#include <Protocol/GraphicsOutput.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include "eficonsole.hpp"
 #include "log.hpp"
 #include "x86.hpp"
-#include "graphics/graphicsconsole.hpp"
-#include "graphics/pixels.hpp"
-#include "graphics/surface.hpp"
 
 
 EFI_HANDLE g_efiImage;
 EFI_SYSTEM_TABLE* ST;
 EFI_BOOT_SERVICES* BS;
 
-static EFI_GUID g_efiDevicePathProtocolGuid = EFI_DEVICE_PATH_PROTOCOL_GUID;
-static EFI_GUID g_efiEdidActiveProtocolGuid = EFI_EDID_ACTIVE_PROTOCOL_GUID;
 static EFI_GUID g_efiFileInfoGuid = EFI_FILE_INFO_ID;
-static EFI_GUID g_efiGraphicsOutputProtocolGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
 static EFI_GUID g_efiLoadedImageProtocolGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
 static EFI_GUID g_efiSimpleFileSystemProtocolGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
 
 
-static Surface g_frameBuffer;
-static GraphicsConsole g_graphicsConsole;
+void InitDisplays();
 
 
 
@@ -67,145 +58,6 @@ static void* AllocatePages(size_t pageCount, physaddr_t maxAddress)
 
     return (void*)memory;
 }
-
-
-
-static PixelFormat DeterminePixelFormat(const EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* info)
-{
-    // TODO
-    // if (info->PixelFormat == PixelRedGreenBlueReserved8BitPerColor)
-    // {
-    //     return PIXFMT_X8B8G8R8;
-    // }
-    //else
-    if (info->PixelFormat == PixelBlueGreenRedReserved8BitPerColor)
-    {
-        return PIXFMT_X8R8G8B8;
-    }
-    // TODO
-    // else if (info->PixelFormat == PixelBitMask)
-    // {
-    //     return DeterminePixelFormat(
-    //         info->PixelInformation.RedMask,
-    //         info->PixelInformation.GreenMask,
-    //         info->PixelInformation.BlueMask,
-    //         info->PixelInformation.ReservedMask
-    //     );
-    // }
-    else
-    {
-        return PIXFMT_UNKNOWN;
-    }
-}
-
-
-
-static void InitDisplay(EFI_GRAPHICS_OUTPUT_PROTOCOL* gop, EFI_EDID_ACTIVE_PROTOCOL* edid)
-{
-    // Start with the current mode as the "best"
-    auto bestModeIndex = gop->Mode->Mode;
-    auto bestModeInfo = *gop->Mode->Info;
-
-    // TODO: use edid information if available to determine optimal resolution
-    (void)edid;
-    const auto maxWidth = bestModeInfo.HorizontalResolution;
-    const auto maxHeight = bestModeInfo.VerticalResolution;
-
-    for (unsigned i = 0; i != gop->Mode->MaxMode; ++i)
-    {
-        EFI_GRAPHICS_OUTPUT_MODE_INFORMATION* info;
-        UINTN size = sizeof(info);
-
-        auto status = gop->QueryMode(gop, i, &size, &info);
-        if (EFI_ERROR(status)) continue;
-
-        const PixelFormat pixfmt = DeterminePixelFormat(info);
-        if (pixfmt == PIXFMT_UNKNOWN) continue;
-
-        // We want to keep the highest resolution possible, but not exceed the "ideal" one.
-        if (info->HorizontalResolution > maxWidth || info->VerticalResolution > maxHeight) continue;
-
-        if (info->HorizontalResolution > bestModeInfo.HorizontalResolution ||
-            info->VerticalResolution > bestModeInfo.VerticalResolution)
-        {
-            bestModeIndex = i;
-            bestModeInfo = *info;
-        }
-    }
-
-    if (gop->Mode->Mode != bestModeIndex)
-    {
-        gop->SetMode(gop, bestModeIndex);
-    }
-}
-
-
-
-static bool InitDisplays()
-{
-    UINTN size = 0;
-    EFI_HANDLE* handles = nullptr;
-    EFI_STATUS status;
-
-    while ((status = BS->LocateHandle(ByProtocol, &g_efiGraphicsOutputProtocolGuid, nullptr, &size, handles)) == EFI_BUFFER_TOO_SMALL)
-    {
-        if (handles) BS->FreePool(handles);
-        status = BS->AllocatePool(EfiLoaderData, size, (void**)&handles);
-        if (EFI_ERROR(status)) break;
-    }
-
-    if (EFI_ERROR(status))
-    {
-        Log("*** Failed to retrieve graphic displays: %x\n", status);
-        return false;
-    }
-
-    const int count = size / sizeof(EFI_HANDLE);
-
-    for (int i = 0; i != count; ++i)
-    {
-        EFI_DEVICE_PATH_PROTOCOL* dpp = nullptr;
-        BS->HandleProtocol(handles[i], &g_efiDevicePathProtocolGuid, (void**)&dpp);
-
-        // If dpp is NULL, this is the "Console Splitter" driver. It is used to draw on all
-        // screens at the same time and doesn't represent a real hardware device.
-        if (!dpp) continue;
-
-        EFI_GRAPHICS_OUTPUT_PROTOCOL* gop = nullptr;
-        BS->HandleProtocol(handles[i], &g_efiGraphicsOutputProtocolGuid, (void**)&gop);
-
-        EFI_EDID_ACTIVE_PROTOCOL* edid = nullptr;
-        BS->HandleProtocol(handles[i], &g_efiEdidActiveProtocolGuid, (void**)&edid);
-
-        InitDisplay(gop, edid);
-
-        if (g_console != &g_graphicsConsole)
-        {
-            auto info = gop->Mode->Info;
-            auto pixfmt = DeterminePixelFormat(info);
-            g_frameBuffer.width = info->HorizontalResolution;
-            g_frameBuffer.height = info->VerticalResolution;
-            g_frameBuffer.pitch = info->PixelsPerScanLine * GetPixelDepth(pixfmt);
-            g_frameBuffer.pixels = (void*)(uintptr_t)gop->Mode->FrameBufferBase;
-            g_frameBuffer.format = pixfmt;
-            g_graphicsConsole.Initialize(&g_frameBuffer);
-            g_graphicsConsole.Clear();
-            g_console = &g_graphicsConsole;
-        }
-
-        Log("    Display #%d:\n", i);
-        Log("        GOP        : %p\n", gop);
-        Log("        EDID       : %p\n", edid);
-        Log("        Resolution : %d x %d\n", gop->Mode->Info->HorizontalResolution, gop->Mode->Info->VerticalResolution);
-        Log("        Framebuffer: %X\n", gop->Mode->FrameBufferBase);
-        Log("        Size       : %X\n", gop->Mode->FrameBufferSize);
-    }
-
-    BS->FreePool(handles);
-
-    return true;
-}
-
 
 
 // Look at this code and tell me EFI isn't insane
