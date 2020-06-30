5ce860486cdba0c6fc61d3134cee01959259f442
Thierry Tremblay <kiznit@users.noreply.github.com>
2020-06-30 09:40:19 -0700

    Fix compile issue with utprint.c

--- services/acpi/module.mk
+++ services/acpi/module.mk
@@ -1,6 +1,11 @@
+ACPICA_IGNORE = \
+	acpica/source/components/resources/rsdump.c \
+	acpica/source/components/utilities/utprint.c
+
+#TODO: I am pretty sure I am compiling and linking things I don't need here...
 ACPICA_DIRS = dispatcher events executer hardware namespace parser resources tables utilities
 ACPICA_GLOB = $(foreach dir,$(ACPICA_DIRS),$(shell find $(ACPICA_PATH)/source/components/$(dir) -name '*.c'))
-ACPICA_SOURCES = $(filter-out acpica/source/components/resources/rsdump.c,$(subst $(ACPICA_PATH),acpica,$(ACPICA_GLOB)))
+ACPICA_SOURCES = $(filter-out $(ACPICA_IGNORE),$(subst $(ACPICA_PATH),acpica,$(ACPICA_GLOB)))
 
 SOURCES := \
 	acpi.c \
